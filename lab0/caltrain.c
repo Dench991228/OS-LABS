9b75e2a831dda224f1b12410df69a31c99365197
dhz <denghanzhi85139@163.com>
2020-09-25 18:51:51 +0800

    每一道题都用不超过一个锁实现

--- lab0/caltrain.c
+++ lab0/caltrain.c
@@ -9,11 +9,11 @@ struct station {
 	int people_aboard;
 	// whether the train is ready to go
 	int train_ready;
+	//the lock that controls everyone
+	struct lock *lock_station;
 	// the lock and condition of the train's movement
-	struct lock *lock_train;
 	struct condition *cond_train;
 	// the lock and condition that controls the passenger's movement
-	struct lock *lock_passenger;
 	struct condition *cond_passenger;
 };
 
@@ -22,15 +22,12 @@ void station_init(struct station *station)
 {
 	// initialize the number of people waiting on the platform
 	station->people_wait = 0;
-	//initialize the lock of passenger
-	station->lock_passenger = (struct lock*)malloc(sizeof(struct lock));
-	lock_init(station->lock_passenger);
+	// initialize the lock of the station
+	station->lock_station = (struct lock*)malloc(sizeof(struct lock));
+	lock_init(station->lock_station);
 	// initialize the condition of passenger
 	station->cond_passenger = (struct condition*)malloc(sizeof(struct condition));
 	cond_init(station->cond_passenger);
-	// initialize the lock of train
-	station->lock_train = (struct lock*)malloc(sizeof(struct lock));
-	lock_init(station->lock_train);
 	// initialize the condition of train
 	station->cond_train = (struct condition*)malloc(sizeof(struct condition));
 	cond_init(station->cond_train);
@@ -42,47 +39,47 @@ void station_load_train(struct station *station, int count)
 	if(count==0)return;
 	if(station->people_wait==0)return;
 	// train cannot leave the station when loading passenger
-	lock_acquire(station->lock_train);
 	int i = 0;
 	station->people_awake = 0;
 	station->people_aboard = 0;
 	station->train_ready = 0;
 	while(i<count&&station->people_wait>0){
-		lock_acquire(station->lock_passenger);
-		cond_signal(station->cond_passenger, station->lock_passenger);
+		lock_acquire(station->lock_station);
+		cond_signal(station->cond_passenger, station->lock_station);
 		station->people_wait--;
 		station->people_awake++;
 		i++;
 		if(!(i<count&&station->people_wait>0))station->train_ready=1;
 		//printf("ready!\n");
-		lock_release(station->lock_passenger);
+		lock_release(station->lock_station);
 		//printf("waking number %d passenger\n", i);
 	}
 	// wait for all people aboard
-	cond_wait(station->cond_train, station->lock_train);
-	lock_release(station->lock_train);
+	lock_acquire(station->lock_station);
+	cond_wait(station->cond_train, station->lock_station);
+	lock_release(station->lock_station);
 }
 
 // a passenger waits on the condition of moving
 void station_wait_for_train(struct station *station)
 {
-	lock_acquire(station->lock_passenger);
+	lock_acquire(station->lock_station);
 	station->people_wait++;
-	cond_wait(station->cond_passenger, station->lock_passenger);
-	lock_release(station->lock_passenger);
+	cond_wait(station->cond_passenger, station->lock_station);
+	lock_release(station->lock_station);
 }
 
 void station_on_board(struct station *station)
 {
-	lock_acquire(station->lock_passenger);
+	lock_acquire(station->lock_station);
 	// really send one passenger on board
 	station->people_aboard++;
 	//printf("passenger No.%d get on board\n", station->people_aboard);
-	lock_release(station->lock_passenger);
+	lock_release(station->lock_station);
 	if(station->people_aboard==station->people_awake&&station->train_ready==1){
 		//printf("ready to go!\n");
-		lock_acquire(station->lock_train);
-		cond_signal(station->cond_train, station->lock_train);
-		lock_release(station->lock_train);
+		lock_acquire(station->lock_station);
+		cond_signal(station->cond_train, station->lock_station);
+		lock_release(station->lock_station);
 	}
 }
--- lab0/reaction.c
+++ lab0/reaction.c
@@ -15,14 +15,8 @@ struct reaction {
 	// the lock of the reaction
 	struct lock *reaction_lock;
 
-	// the lock of modifying the number of hydrogen atom
-	struct lock *hydrogen_lock;
-
 	// the condition where hydrogen atoms are waiting for reaction
 	struct condition *hydrogen_cond;
-
-	// the lock of modifying the number oxygen
-	struct lock *oxygen_lock;
 
 	// the condition where oxygen atoms are waiting for reaction
 	struct condition *oxygen_cond;
@@ -38,14 +32,10 @@ void reaction_init(struct reaction *reaction)
 	// initialize the lock of reaction
 	reaction->reaction_lock = (struct lock*)malloc(sizeof(struct lock));
 	lock_init(reaction->reaction_lock);
-	// initialize the lock and condition of hydrogen
-	reaction->hydrogen_lock = (struct lock*)malloc(sizeof(struct lock));
-	lock_init(reaction->hydrogen_lock);
+	// initialize the condition of hydrogen
 	reaction->hydrogen_cond = (struct condition*)malloc(sizeof(struct condition));
 	cond_init(reaction->hydrogen_cond);
-	// initialize the lock and condition of oxygen
-	reaction->oxygen_lock = (struct lock*)malloc(sizeof(struct lock));
-	lock_init(reaction->oxygen_lock);
+	// initialize the condition of oxygen
 	reaction->oxygen_cond = (struct condition*)malloc(sizeof(struct condition));
 	cond_init(reaction->oxygen_cond);
 }
@@ -55,32 +45,20 @@ void reaction_init(struct reaction *reaction)
 void reaction_h(struct reaction *reaction)
 {
 	lock_acquire(reaction->reaction_lock);
-	// reaction can takes places
 	if(reaction->hydrogen_number >= 1 && reaction->oxygen_number >= 1){
-		// consume a hydrogen atom
-		lock_acquire(reaction->hydrogen_lock);
-		cond_signal(reaction->hydrogen_cond, reaction->hydrogen_lock);
+		// consume a hydrogen atom and an oxygen atom
+		cond_signal(reaction->hydrogen_cond, reaction->reaction_lock);
 		reaction->hydrogen_number--;
-		lock_release(reaction->hydrogen_lock);
-
-		// consume an oxygen atom
-		lock_acquire(reaction->oxygen_lock);
-		cond_signal(reaction->oxygen_cond, reaction->oxygen_lock);
+		cond_signal(reaction->oxygen_cond, reaction->reaction_lock);
 		reaction->oxygen_number--;
-		lock_release(reaction->oxygen_lock);
 
-		// forms a water molecule
 		make_water();
-
-		lock_release(reaction->reaction_lock);
 	}
 	else{// no reaction
-		lock_release(reaction->reaction_lock);
-		lock_acquire(reaction->hydrogen_lock);
 		reaction->hydrogen_number++;
-		cond_wait(reaction->hydrogen_cond, reaction->hydrogen_lock);
-		lock_release(reaction->hydrogen_lock);
+		cond_wait(reaction->hydrogen_cond, reaction->reaction_lock);
 	}
+	lock_release(reaction->reaction_lock);
 }
 
 void reaction_o(struct reaction *reaction)
@@ -88,23 +66,15 @@ void reaction_o(struct reaction *reaction)
 	// reaction takes place
 	lock_acquire(reaction->reaction_lock);
 	if(reaction->hydrogen_number>=2){
-		// consume two hydrogen atoms
-		lock_acquire(reaction->hydrogen_lock);
-		cond_signal(reaction->hydrogen_cond, reaction->hydrogen_lock);
-		cond_signal(reaction->hydrogen_cond, reaction->hydrogen_lock);
+		//consume two hydrogen atoms
+		cond_signal(reaction->hydrogen_cond, reaction->reaction_lock);
+		cond_signal(reaction->hydrogen_cond, reaction->reaction_lock);
 		reaction->hydrogen_number-=2;
-		lock_release(reaction->hydrogen_lock);
-
-		// forms a water molecule
 		make_water();
-
-		lock_release(reaction->reaction_lock);
 	}
 	else{// no reaction
-		lock_release(reaction->reaction_lock);
-		lock_acquire(reaction->oxygen_lock);
 		reaction->oxygen_number++;
-		cond_wait(reaction->oxygen_cond, reaction->oxygen_lock);
-		lock_release(reaction->oxygen_lock);
+		cond_wait(reaction->oxygen_cond, reaction->reaction_lock);
 	}
+	lock_release(reaction->reaction_lock);
 }
